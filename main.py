e36a8bf1e7ae3c77fb56cecf5f2794099d87e864
OgurchickRick <artem.derbin.04@list.ru>
2022-01-26 22:22:47 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import os
 
+
+def path():
+    while True:
+        way = str(input('Введите путь до папки:'))
+        if os.path.exists(way) is True:
+            return way
+        else:
+            print("Неверный путь")
+
+
+def dictionary(way):
+    a = {}
+    for filename in os.listdir(way):
+        a.update({way + '\\' + filename: os.path.getsize(way + '\\' + filename)})
+    print(a)
+
+
+def duplicate():
+    pass
+
+
+def print_duplicate():
+    pass
+
+
+if __name__ == '__main__':
+    p = path()
+    dictionary(p)
