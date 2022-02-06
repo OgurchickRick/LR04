72ee68df68f50895694bc6b44f44d280dacdb30a
OgurchickRick <artem.derbin.04@list.ru>
2022-02-06 21:32:32 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 
-def path():
+def way():
     while True:
         way = str(input('Введите путь до папки:'))
         if os.path.exists(way):
@@ -10,13 +10,15 @@ def path():
             print("Неверный путь")
 
 
-def dictionary(way, a={}):
+def dictionary(way):
+    result = {}
     for filename in os.listdir(way):
-        if os.path.isdir(way + '\\' + filename):
-            dictionary(way + '\\' + filename)
+        current_path = os.path.join(way, filename)
+        if os.path.isdir(current_path):
+            result.update(dictionary(current_path))
         else:
-            a.update({way + '\\' + filename: os.path.getsize(way + '\\' + filename)})
-    return a
+            result.update({current_path: os.path.getsize(current_path)})
+    return result
 
 
 def duplicate(a):
@@ -25,7 +27,7 @@ def duplicate(a):
         for key2 in a:
             if key1 == key2:
                 pass
-            elif key1[key1.rfind('\\'):] == key2[key2.rfind('\\'):] and a[key1] == a[key2]:
+            elif os.path.basename(key1) == os.path.basename(key2) and a[key1] == a[key2]:
                 if key1 in copies:
                     continue
                 copies.update({key1: a[key1]})
@@ -33,25 +35,24 @@ def duplicate(a):
         return copies
 
 
-
 def print_duplicate(copies):
     if copies == {}:
         print('Нет дубликатов')
-    hlam = []
+    garbage = []
     for key1 in copies:
-        if key1 not in hlam:
+        if key1 not in garbage:
             print('\n', '-----', os.path.getsize(key1), 'байт -----')
             print(key1)
             for key2 in copies:
                 if key1 == key2:
                     pass
-                elif key1[key1.rfind('\\'):] == key2[key2.rfind('\\'):] and copies[key1] == copies[key2]:
+                elif os.path.basename(key1) == os.path.basename(key2) and copies[key1] == copies[key2]:
                     print(key2)
-                    hlam.append(key2)
+                    garbage.append(key2)
 
 
 if __name__ == '__main__':
-    p = path()
+    p = way()
     s = dictionary(p)
     duplicate(s)
     d = duplicate(s)
